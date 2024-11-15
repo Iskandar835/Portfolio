d9833f15481c23666cdba563c26408435b0354bf
Sacha Vandermoeten <sachadu835@gmail.com>
2024-11-15 16:19:19 +0100

    section Project full optimiser pour media tabs

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import PropTypes from "prop-types";
 import Logo from "./TechnoLogo";
+import { devices } from "../helpers/Breakpoints";
 
 
 const CardLink = styled.a`
@@ -8,32 +9,10 @@ const CardLink = styled.a`
     width: 340px;
     height: 300px;
     text-decoration: none;
-`
-const CardContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    position: relative;
-    width: 100%;
-    height: 300px;
-    overflow: hidden; 
-    background-color: #FFFFFF;
-    border-radius: 15px;
-    box-shadow: 2px 2px 8px 2px rgba(0, 0, 0, 0.1);
-    transition: transform 175ms ease-out, box-shadow 175ms ease-out;
-    &:hover {
-        transform: scale(1.01);
-        box-shadow: 3px 3px 16px 9px rgba(0, 0, 0, 0.1);
+    @media ${devices.tabs} {
+        width: 430px;
+        height: 330px;
     }
-    &:hover > .card-content {
-        top: 210px;
-    }
-`
-const CardImg = styled.img`
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    border-radius: 15px;
-    object-fit: cover;
 `
 const CardContent = styled.div`
     position: relative;
@@ -48,14 +27,44 @@ const CardContent = styled.div`
     background-color: ##6e6e7300;
     border-radius: 0 0 15px 15px;
     transition: top 175ms ease-out;
+    @media ${devices.tabs} {
+        top: 235px;
+    }
 `
-
+const CardContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    width: 100%;
+    height: 100%;
+    overflow: hidden; 
+    background-color: #FFFFFF;
+    border-radius: 15px;
+    box-shadow: 2px 2px 8px 2px rgba(0, 0, 0, 0.1);
+    transition: transform 175ms ease-out, box-shadow 175ms ease-out;
+    &:hover {
+        transform: scale(1.01);
+        box-shadow: 3px 3px 16px 9px rgba(0, 0, 0, 0.1);
+    }
+    &:hover {
+        ${CardContent} {
+            top: 210px;
+        }
+    }
+`
+const CardImg = styled.img`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    border-radius: 15px;
+    object-fit: cover;
+`
 function CardAlone ({ projet }) {
     return (
         <CardLink href={projet.link} target="_blank">
             <CardContainer>
                 <CardImg src={projet.picture} alt={projet.name}/>
-                <CardContent className="card-content">
+                <CardContent>
                     {projet.technos.map((techno, index) => (
                         <Logo key={index} className={techno}/>
                     ))}
--- src/components/PageStructure/Projects.jsx
+++ src/components/PageStructure/Projects.jsx
@@ -2,23 +2,36 @@ import styled from "styled-components";
 import CardAlone from "../Card";
 import Data from "../../data/database.json";
 import { Span } from "./BannerContact";
+import { devices } from "../../helpers/Breakpoints";
 
 
 const ProjectSection = styled.section`
     display: flex;
     flex-direction: column;
     margin: 200px 50px 0 50px;
+    @media ${devices.tabs} {
+        margin: 150px 80px 0 80px;
+    }
 `
 export const Titleh2 = styled.h2`
     font-family: "Poppins", sans-serif;
     font-size: 60px;
     text-wrap: nowrap;
     text-align: center;
+    @media ${devices.tabs} {
+        margin: 0 0 80px 0;
+        text-align: center;
+    }
 `
 const AllCards = styled.div`
     display: flex;
     justify-content: center;
     gap: 50px;
+    @media ${devices.tabs} {
+        align-items: center;
+        flex-direction: column;
+        gap: 70px;
+    }
 `
 
 function Projects () {
