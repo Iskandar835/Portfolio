f85e4d47c2b8a229cf90a9a0a60fee8cdf339657
Sacha Vandermoeten <sachadu835@gmail.com>
2024-11-28 16:18:14 +0100

    bug backdrop-filter fixe

--- src/components/PageStructure/BannerContact.jsx
+++ src/components/PageStructure/BannerContact.jsx
@@ -28,9 +28,8 @@ const ContactSection = styled.section`
         left: 0;
         width: 100%;
         height: 100%;
-        background: url("assets/doodle-items.avif"); 
-        background-color: rgba(255, 255, 255, 0.6)!important;
-        filter: blur(2px);
+        background: url("assets/doodle-items.jpg");
+        filter: blur(1.7px);
         z-index: 0;
     }
     @media ${devices.tabs} {
