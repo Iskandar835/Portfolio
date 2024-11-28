f16defc3b98f87db05e5a67b3cbd362af097498c
Sacha Vandermoeten <sachadu835@gmail.com>
2024-11-28 16:18:28 +0100

    Updates

--- assets/index-B7bdGSX5.js
+++ assets/index-B7bdGSX5.js
@@ -439,9 +439,8 @@ Error generating stack: `+i.message+`
         left: 0;
         width: 100%;
         height: 100%;
-        background: url("assets/doodle-items.avif"); 
-        background-color: rgba(255, 255, 255, 0.6)!important;
-        filter: blur(2px);
+        background: url("assets/doodle-items.jpg");
+        filter: blur(1.7px);
         z-index: 0;
     }
     @media ${$.tabs} {
